b25cb8fef1b6343528a19c0e76cb24d5f9ae8432
chingkii <51781069+chingkii@users.noreply.github.com>
2019-06-13 16:36:57 +0700

    Update 2wordcount.py

--- 2wordcount.py
+++ 2wordcount.py
@@ -6,7 +6,7 @@ def main():
      sc = SparkContext(appName="2WordCount")
      input_file = sc.textFile('/user/cloudera/wp/wordcount.txt')
      token = input_file.map(lambda line : line.strip().split(" "))
-     words = token.flatMap(Lambda xs : (tuple(x) for x in zip((xs, xs[1:])))
+     words = token.flatMap(lambda xs : (tuple(x) for x in zip(xs, xs[1:])))
      biwords = words.map(lambda x: (x, 1))
      wp = biwords.reduceByKey(lambda a, b: a + b)
      wp.saveAsTextFile('/user/cloudera/wp/output')
